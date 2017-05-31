f5a00e1c3b8a2fd0c634933801b949087ba5c0d1
fish1996 <370299459@qq.com>
2017-05-31 22:58:52 +0800

    更新

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,74 +1,66 @@
 package com.example.administrator.myapplication;
-import com.baidu.speech.VoiceRecognitionService;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.speech.SpeechRecognizer;
-import android.speech.RecognitionListener;
-import android.widget.Button;
+import android.view.MotionEvent;
 import android.view.View;
-class MainActivity extends AppCompatActivity implements RecognitionListener {
-    private SpeechRecognizer speechRecognizer;
-    private Button okBtn;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SpeechRecognizerTool.ResultsCallback {
+
+    private Button mStartSpeechButton;
+    private TextView mTextView;
+
+    private SpeechRecognizerTool mSpeechRecognizerTool = new SpeechRecognizerTool(this);
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initView();
         setContentView(R.layout.content_main);
 
-        // 创建识别器
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this, new ComponentName(this, VoiceRecognitionService.class));
-        // 注册监听器
-        speechRecognizer.setRecognitionListener(this);
-    }
+        mTextView = (TextView) findViewById(R.id.speechTextView);
 
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.okButton:{
-                startASR();
-                break;
+        mStartSpeechButton = (Button) findViewById(R.id.startSpeechButton);
+        mStartSpeechButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        mSpeechRecognizerTool.startASR(MainActivity.this);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizerTool.stopASR();
+                        break;
+                    default:
+                        return false;
+                }
+
+                return true;
             }
+        });
+    }
 
-        }
-    }
-    void initView(){
-        okBtn = (Button)findViewById(R.id.okButton);
-    }
-    // 开始识别
-    void startASR() {
-        Intent intent = new Intent();
-        bindParams(intent);
-        speechRecognizer.startListening(intent);
-    }
-    void bindParams(Intent intent) {
-        // 设置识别参数
-    }
-    public void onReadyForSpeech(Bundle params) {
-        // 准备就绪
-    }
     @Override
-    public void onBeginningOfSpeech() {
-        // 开始说话处理
+    protected void onStart() {
+        super.onStart();
+        mSpeechRecognizerTool.createTool();
     }
-    public void onRmsChanged(float rmsdB) {
-        // 音量变化处理
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSpeechRecognizerTool.destroyTool();
     }
-    public void onBufferReceived(byte[] buffer) {
-        // 录音数据传出处理
-    }
-    public void onEndOfSpeech() {
-        // 说话结束处理
-    }
-    public void onError(int error) {
-        // 出错处理
-    }
-    public void onResults(Bundle results) {
-        // 最终结果处理
-    }
-    public void onPartialResults(Bundle partialResults) {
-        // 临时结果处理
-    }
-    public void onEvent(int eventType, Bundle params) {
-        // 处理事件回调
+
+    @Override
+    public void onResults(String result) {
+        final String finalResult = result;
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText(finalResult);
+            }
+        });
     }
 }
